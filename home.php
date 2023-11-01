9145d25a20e62233c90d3bb41b953b1758fb9641
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-01 15:36:25 -0600

    Improves the UI of home page

--- home.php
+++ home.php
@@ -44,8 +44,8 @@ if ($photosCount == 0 && $currentPage > 1) {
         </div>
     </header>
 
-    <div class="fotos">
-        <div class="contenedor">
+    <div class="contenedor">
+        <div class="photos-grid">
             <?php foreach ($photos as $photo) { ?>
                 <div class="thumb">
                     <a href="photo.php?id=<?php echo $photo['id'] ?>">
@@ -53,16 +53,18 @@ if ($photosCount == 0 && $currentPage > 1) {
                     </a>
                 </div>
             <?php } ?>
+        </div>
 
-            <div class="paginacion">
-                <?php if ($currentPage > 1) { ?>
-                    <a href="index.php?p=<?php echo $currentPage - 1 ?>" class="izquierda"><i class="fa fa-long-arrow-left"></i> Pagina Anterior</a>
-                <?php } ?>
+        <div class="pagination">
+            <?php if ($currentPage > 1) { ?>
+                <a href="index.php?p=<?php echo $currentPage - 1 ?>" class="pagination-button"><i
+                        class="fa fa-long-arrow-left"></i> Pagina Anterior</a>
+            <?php } ?>
 
-                <?php if ($currentPage != $pagesCount && $photosCount > $photosPerPage) { ?>
-                    <a href="index.php?p=<?php echo $currentPage + 1 ?>" class="derecha">Pagina Siguiente <i class="fa fa-long-arrow-right"></i></a>
-                <?php } ?>
-            </div>
+            <?php if ($currentPage != $pagesCount && $photosCount > $photosPerPage) { ?>
+                <a href="index.php?p=<?php echo $currentPage + 1 ?>" class="pagination-button">Pagina Siguiente <i
+                        class="fa fa-long-arrow-right"></i></a>
+            <?php } ?>
         </div>
     </div>
 
