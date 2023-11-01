df97e471f59cad2bc42120abc0868eb6d869251c
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-31 22:03:00 -0600

    Define the home page template and the css styles

---
+++ views/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <title>Mi Increíble Galería en PHP</title>
+    <link rel="stylesheet" href="public/css/styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Mi Increíble Galería en PHP y MySQL</h1>
+        </div>
+    </header>
+
+    <div class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="#">
+                    <img src="uploaded/1.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="paginacion">
+                <a href="#" class="izquierda"><i class="fa fa-long-arrow-left"></i> Pagina Anterior</a>
+                <a href="#" class="derecha">Pagina Siguiente <i class="fa fa-long-arrow-right"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p class="copyright">Galeria creada por Jozuan Martínez - 2023</p>
+    </footer>
+</body>
+
+</html>
