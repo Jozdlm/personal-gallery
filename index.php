0fa5f84562d455f31b529a6fc533273f0ae1a944
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-31 22:56:02 -0600

    Configure the dot-env package at index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+require_once "src/DbConnection.php";
+
+if (!getDbConnection()) {
+    header("Location: error.php");
+}
+
 require_once("home.php");
