224108b5b2036daa3a589c3a8a934b7b4b41efb2
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-31 22:54:57 -0600

    Define the error page for any errors on the app

---
+++ error.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <title>Mi Increíble Galería en PHP</title>
+    <link rel="stylesheet" href="public/css/styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">ERROR</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+        <p>Hemos tenido un error al intentar carga la aplicación</p>
+    </div>
+
+    <footer>
+        <p class="copyright">Galeria creada por Jozuan Martínez - 2023</p>
+    </footer>
+</body>
+
+</html>
