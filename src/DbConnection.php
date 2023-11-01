4f561a9ba990bf2c7897ae069369d49ccc8f5a1e
Jozuan Lopez <jozuanlopez@gmail.com>
2023-10-31 22:48:20 -0600

    Define a connection to the database and returned

---
+++ src/DbConnection.php
@@ -0,0 +1,15 @@
+<?php
+
+function getDbConnection(): PDO
+{
+    $host = $_ENV["DB_HOST"];
+    $dbName = $_ENV["DB_NAME"];
+    $username = $_ENV["DB_USER"];
+    $password = $_ENV["DB_PASS"];
+
+    try {
+        return new PDO("mysql:host=$host; dbname=$dbName", $username, $password);
+    } catch (\PDOException $pdoException) {
+        throw $pdoException;
+    }
+}
