0c177c26afb3ed4674afe17fc50c86fa96eda9f0
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-01 20:09:20 -0600

    Handles the post request and updates the photo

--- photo.php
+++ photo.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("src/PhotoRepository.php");
+require_once("src/UploadService.php");
 
 $id = isset($_GET['id']) ? (int) $_GET['id'] : false;
 $photo = [];
@@ -9,12 +10,26 @@ if (!$id) {
     header("Location:index.php");
 }
 
+// Executes when it send a POST request and before we get the photo details
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
+    $isAnImage = getimagesize($_FILES['photo']['tmp_name']);
+
+    if ($isAnImage && isset($_POST['title']) && isset($_POST['description'])) {
+        $imgUrl = uploadPhoto();
+
+        $newValues = ['title' => $_POST['title'], 'description' => $_POST['description'], 'imgUrl' => $imgUrl];
+        updatePhoto($id, $newValues);
+
+        unlink($_SERVER['DOCUMENT_ROOT'] . '/' . $_POST['old_img']);
+        header('Location:index.php');
+    }
+}
+
 $photo = findPhotoById($id);
 
 if ($id && empty($photo)) {
     header("Location:index.php");
 }
-
 ?>
 
 <!DOCTYPE html>
