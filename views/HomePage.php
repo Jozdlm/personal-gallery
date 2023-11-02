751c695dd8f5c9e8a5b7ffc088398c1b82e51a41
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-01 22:03:13 -0600

    Uses the HTML partials files at home page

--- views/HomePage.php
+++ views/HomePage.php
@@ -1,53 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Head.php') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <title>Mi Increíble Galería en PHP</title>
-    <link rel="stylesheet" href="public/css/styles.css">
-</head>
-
-<body>
-    <header>
-        <div class="contenedor">
-            <div class="header-wrapper">
-                <h1 class="header-title">Hola, aquí están tus fotos!</h1>
-                <a href="upload.php" class="header-cta"><i class="fa fa-upload"></i>Subir Foto</a>
-            </div>
-        </div>
-    </header>
-
+<header>
     <div class="contenedor">
-        <div class="photos-grid">
-            <?php foreach ($photos as $photo) { ?>
-                <div class="thumb">
-                    <a href="photo.php?id=<?php echo $photo['id'] ?>">
-                        <img src="<?php echo $photo['img_url'] ?>" alt="<?php echo $photo['title'] ?>">
-                    </a>
-                </div>
-            <?php } ?>
-        </div>
-
-        <div class="pagination">
-            <?php if ($currentPage > 1) { ?>
-                <a href="index.php?p=<?php echo $currentPage - 1 ?>" class="pagination-button"><i
-                        class="fa fa-long-arrow-left"></i> Pagina Anterior</a>
-            <?php } ?>
-
-            <?php if ($currentPage != $pagesCount && $photosCount > $photosPerPage) { ?>
-                <a href="index.php?p=<?php echo $currentPage + 1 ?>" class="pagination-button">Pagina Siguiente <i
-                        class="fa fa-long-arrow-right"></i></a>
-            <?php } ?>
+        <div class="header-wrapper">
+            <h1 class="header-title">Hola, aquí están tus fotos!</h1>
+            <a href="upload.php" class="header-cta"><i class="fa fa-upload"></i>Subir Foto</a>
         </div>
     </div>
+</header>
 
-    <footer>
-        <p class="copyright">Galeria creada por Jozuan Martínez - 2023</p>
-    </footer>
-</body>
+<div class="contenedor">
+    <div class="photos-grid">
+        <?php foreach ($photos as $photo) { ?>
+            <div class="thumb">
+                <a href="photo.php?id=<?php echo $photo['id'] ?>">
+                    <img src="<?php echo $photo['img_url'] ?>" alt="<?php echo $photo['title'] ?>">
+                </a>
+            </div>
+        <?php } ?>
+    </div>
 
-</html>
+    <div class="pagination">
+        <?php if ($currentPage > 1) { ?>
+            <a href="index.php?p=<?php echo $currentPage - 1 ?>" class="pagination-button"><i
+                    class="fa fa-long-arrow-left"></i> Pagina Anterior</a>
+        <?php } ?>
+
+        <?php if ($currentPage != $pagesCount && $photosCount > $photosPerPage) { ?>
+            <a href="index.php?p=<?php echo $currentPage + 1 ?>" class="pagination-button">Pagina Siguiente <i
+                    class="fa fa-long-arrow-right"></i></a>
+        <?php } ?>
+    </div>
+</div>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Footer.php') ?>
