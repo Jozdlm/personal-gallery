dd1ff70c610c0dc0a255c25b11fbc07e4d8072d6
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-01 21:05:13 -0600

    Defines the photo details page file that contains the HTML template

--- photo.php
+++ photo.php
@@ -10,7 +10,7 @@ if (!$id) {
     header("Location:index.php");
 }
 
-// Executes when it send a POST request and before we get the photo details
+// Runs when it sends a POST request and before we get the photo details
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_FILES)) {
     $isAnImage = getimagesize($_FILES['photo']['tmp_name']);
 
@@ -29,50 +29,5 @@ $photo = findPhotoById($id);
 if ($id && empty($photo)) {
     header("Location:index.php");
 }
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <title>Mi Increíble Galería en PHP</title>
-    <link rel="stylesheet" href="public/css/styles.css">
-</head>
-
-<body>
-    <header>
-        <div class="contenedor">
-            <h1 class="photo-title">
-                <?php if (!empty($photo['title'])) {
-                    echo $photo['title'];
-                } ?>
-            </h1>
-        </div>
-    </header>
-    
-    <div class="contenedor">
-        <div class="foto">
-            <img src="<?php echo $photo['img_url'] ?>" alt="<?php echo $photo['title'] ?>">
-            <p class="texto">
-                <?php if (!empty($photo['description'])) {
-                    echo $photo['description'];
-                } ?>
-            </p>
-            <div class="photo-footer">
-                <a href="index.php" class="back-button"><i class="fa fa-long-arrow-left"></i>Regresar</a>
-                <a href="edit-photo.php?id=<?php echo $id ?>" class="back-button"><i class="fa fa-pencil-square-o"></i>Actualizar</a>
-            </div>
-        </div>
-    </div>
-
-    <footer>
-        <p class="copyright">Galeria creada por Jozuan Martínez - 2023</p>
-    </footer>
-</body>
-
-</html>
+require_once("views/PhotoDetailsPage.php");
---
+++ views/PhotoDetailsPage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <title>Mi Increíble Galería en PHP</title>
+    <link rel="stylesheet" href="public/css/styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="photo-title">
+                <?php if (!empty($photo['title'])) {
+                    echo $photo['title'];
+                } ?>
+            </h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+        <div class="foto">
+            <img src="<?php echo $photo['img_url'] ?>" alt="<?php echo $photo['title'] ?>">
+            <p class="texto">
+                <?php if (!empty($photo['description'])) {
+                    echo $photo['description'];
+                } ?>
+            </p>
+            <div class="photo-footer">
+                <a href="index.php" class="back-button"><i class="fa fa-long-arrow-left"></i>Regresar</a>
+                <a href="edit-photo.php?id=<?php echo $id ?>" class="back-button"><i
+                        class="fa fa-pencil-square-o"></i>Actualizar</a>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p class="copyright">Galeria creada por Jozuan Martínez - 2023</p>
+    </footer>
+</body>
+
+</html>
