345b907f281d10b5534c2a4bb5775b1082747dfb
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-01 22:02:50 -0600

    Defines a common files that it uses along the pages

---
+++ views/Shared/Footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p class="copyright">Galeria creada por Jozuan Martínez - 2023</p>
+</footer>
+</body>
+
+</html>
---
+++ views/Shared/Head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <title>Lindos Recuerdos</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="public/css/styles.css">
+</head>
+
+<body>
