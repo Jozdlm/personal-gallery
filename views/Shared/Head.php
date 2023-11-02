a067a43830f1992efe37a6c312bbd23cce1527e8
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-01 22:14:02 -0600

    Updates the Head partial that allows to have scoped styles

--- views/Shared/Head.php
+++ views/Shared/Head.php
@@ -11,6 +11,10 @@
     <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="public/css/styles.css">
+
+    <?php if (isset($scopedStyle)) { ?>
+        <link rel="stylesheet" href="<?php echo $scopedStyle ?>">
+    <?php } ?>
 </head>
 
 <body>
