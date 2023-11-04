1f5626ab828a56bd8385e1676ad0ecc005525ed5
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-03 21:04:59 -0600

    Validates and creates a new user in to the DB

--- src/UserRepository.php
+++ src/UserRepository.php
@@ -2,6 +2,22 @@
 
 require_once("src/DbConnection.php");
 
+function createUser($user): bool
+{
+    if (!$user["username"]) return false;
+    if (!$user["email"]) return false;
+    if (!$user["password"]) return false;
+
+    $conn = getDbConnection();
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES(:username, :email, :password)");
+
+    return $stmt->execute([
+        ":username" => $user["username"],
+        ":email" => $user["email"],
+        ":password" => $user["password"],
+    ]);
+}
+
 function getUserByEmail($email)
 {
     $conn = getDbConnection();
