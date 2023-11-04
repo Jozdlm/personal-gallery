dc54f9fbddb00b5b1090d424614b796876dfd30f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-04 07:45:58 -0600

    Defines function to create a new client session

--- src/AuthService.php
+++ src/AuthService.php
@@ -7,3 +7,17 @@ function checkIfUserExist($userEmail)
     $user = getUserByEmail($userEmail);
     return ($user) ? true : false;
 }
+
+function createClientSession($id, $username, $email)
+{
+    if(!$username) return;
+    session_start();
+
+    $_SESSION["user"] = [
+        "id" => $id,
+        "username" => $username,
+        "email" => $email,
+    ];
+
+    header("Location: home.php");
+}
