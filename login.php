48196c2843042dcc95ee797dd23f9a35dc2c926f
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-03 22:38:06 -0600

    Allows the user to login to the app

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("src/UserRepository.php");
+require_once("src/AuthService.php");
+
+$message = "";
+
+if (isset($_POST["email"]) && isset($_POST['password'])) {
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $password = $_POST['password'];
+
+    if (checkIfUserExist($email)) {
+        $user = getUserByEmail($email);
+        $hashedPassword = $user["password"];
+
+        if (password_verify($password, $hashedPassword)) {
+            $message = "Welcome to app";
+        } else {
+            $message = "Wrong credentials";
+        }
+    }
+}
+
+require_once("views/LoginPage.php");
