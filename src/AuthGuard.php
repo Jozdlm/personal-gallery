a2caf48d497ef058ae13ae46efa5951a2b7d6552
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-04 19:15:08 -0600

    Defines the is-anon guard, which validates that the user is anonymous

--- src/AuthGuard.php
+++ src/AuthGuard.php
@@ -14,3 +14,18 @@ function isLoggedGuard(): void
         exit;
     }
 }
+
+/**
+ * Validates that the user has an anonymous session, and if not, redirects to the gallery.
+ */
+function isAnonGuard(): void
+{
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    if (isset($_SESSION['user']['id'])) {
+        header("Location: home.php");
+        exit;
+    }
+}
