875033b746b5ae7c09abd6293df91a55b001422d
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-04 21:37:22 -0600

    Changes the algorithm used for photo names

--- src/UploadService.php
+++ src/UploadService.php
@@ -2,8 +2,12 @@
 
 function uploadPhoto(): string
 {
-    $uploadedFolder = 'uploaded/';
-    $imgUrl = $uploadedFolder . $_FILES['photo']['name'];
+    $uploadedPath = 'uploaded/';
+    $userId = $_SESSION['user']['id'];
+    $uniqueTimeStamp = uniqid();
+    [, $fileExtension] = explode("/", $_FILES['photo']['type']);
+
+    $imgUrl = "{$uploadedPath}user_{$userId}_{$uniqueTimeStamp}.{$fileExtension}";
     move_uploaded_file($_FILES['photo']['tmp_name'], $imgUrl);
 
     return $imgUrl;
