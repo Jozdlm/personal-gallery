e590f7987764eb34c2640daecb9911e5efa008d8
Jozuan Lopez <jozuanlopez@gmail.com>
2023-11-04 21:40:16 -0600

    Changes the title position of the details page

--- views/PhotoDetailsPage.php
+++ views/PhotoDetailsPage.php
@@ -1,23 +1,22 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Head.php') ?>
 
-<header>
-    <div class="contenedor">
-        <h1 class="photo-title">
-            <?php if (!empty($photo['title'])) {
-                echo $photo['title'];
-            } ?>
-        </h1>
-    </div>
-</header>
-
 <div class="contenedor">
     <div class="foto">
         <img src="<?php echo $photo['img_url'] ?>" alt="<?php echo $photo['title'] ?>">
-        <p class="texto">
-            <?php if (!empty($photo['description'])) {
-                echo $photo['description'];
-            } ?>
-        </p>
+
+        <div class="texto">
+            <h1 class="photo-title">
+                <?php if (!empty($photo['title'])) {
+                    echo $photo['title'];
+                } ?>
+            </h1>
+            <p>
+                <?php if (!empty($photo['description'])) {
+                    echo $photo['description'];
+                } ?>
+            </p>
+        </div>
+
         <div class="photo-footer">
             <a href="index.php" class="back-button"><i class="fa fa-long-arrow-left"></i>Regresar</a>
             <a href="edit-photo.php?id=<?php echo $id ?>" class="back-button"><i
