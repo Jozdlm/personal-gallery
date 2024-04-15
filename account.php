4962f02839e453a90bfd60acfcc882d410e8cd90
Jozuan Lopez <jozuanlopez@gmail.com>
2024-04-15 10:59:26 -0600

    Render and send data to the account page

--- account.php
+++ account.php
@@ -1,7 +1,15 @@
 <?php
 require_once "src/Helpers/Router.php";
+require_once "src/AuthService.php";
+require_once "src/AuthGuard.php";
+
+isLoggedGuard();
 
 Router::renderPage([
     "page" => "AccountPage",
-    "layout" => "AppLayout"
+    "layout" => "AppLayout",
+    "customStyle" => "home",
+    "data" => [
+        "clientSession" => getClientSession()
+    ]
 ]);
