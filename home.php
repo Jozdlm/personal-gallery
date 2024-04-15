ba4fd3978da572be8cdcfa2c47ee99f44b65accf
Jozuan Lopez <jozuanlopez@gmail.com>
2024-04-14 22:38:28 -0600

    Renders the page using the router's method

--- home.php
+++ home.php
@@ -1,15 +1,17 @@
 <?php
-require_once("src/AuthGuard.php");
-isLoggedGuard();
+require_once "src/Helpers/HttpParams.php";
+require_once "src/Helpers/Router.php";
+require_once "src/AuthGuard.php";
+require_once "src/PhotoRepository.php";
 
-require_once("src/PhotoRepository.php");
+isLoggedGuard();
 
 $photos = [];
 $photosPerPage = 8;
 $photosCount = 0;
 $pagesCount = 0;
 
-$currentPage = isset($_GET['p']) ? (int) $_GET['p'] : 1;
+$currentPage = (int) HttpParams::get('p') ?? 1;
 $startItems = ($currentPage > 1) ? $currentPage * $photosPerPage - $photosPerPage : 0;
 
 $photos = findPhotosByUser($startItems, $photosPerPage, (int) $_SESSION['user']['id']);
@@ -21,4 +23,15 @@ if ($photosCount == 0 && $currentPage > 1) {
     header('Location:index.php');
 }
 
-require_once('views/HomePage.php');
+Router::renderPage([
+    "page" => "HomePage",
+    "layout" => "AppLayout",
+    "customStyle" => "home",
+    "data" => [
+        "photos" => $photos,
+        "currentPage" => $currentPage,
+        "pagesCount" => $pagesCount,
+        "photosCount" => $photosCount,
+        "photosPerPage" => $photosPerPage
+    ]
+]);
