68f6d5d78ea3dbb88fbbc1bc420b81464faa7f1e
Jozuan Lopez <jozuanlopez@gmail.com>
2024-04-14 20:49:21 -0600

    Switch to conditional block style in the template

--- views/PhotoDetailsPage.php
+++ views/PhotoDetailsPage.php
@@ -1,6 +1,6 @@
 <?php
 $scopedStyle = "public\css\photo-details.css";
-require_once($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Head.php');
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Head.php');
 ?>
 
 <div class="wrapper">
@@ -8,24 +8,26 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Head.php');
         <img src="<?php echo $photo['img_url'] ?>" alt="<?php echo $photo['title'] ?>" class="photo__img">
 
         <div class="photo__body">
-            <h1 class="photo__title">
-                <?php if (!empty($photo['title'])) {
-                    echo $photo['title'];
-                } ?>
-            </h1>
-            <p>
-                <?php if (!empty($photo['description'])) {
-                    echo $photo['description'];
-                } ?>
-            </p>
+            <?php if (!empty($photo['title'])): ?>
+                <h1 class="photo__title">
+                    <?php echo $photo['title'] ?>
+                </h1>
+            <?php endif ?>
+            <?php if (!empty($photo['description'])): ?>
+                <p>
+                    <?php echo $photo['description'] ?>
+                </p>
+            <?php endif ?>
         </div>
 
         <div class="photo__footer">
             <a href="index.php">Regresar</a>
-            <a href="edit-photo.php?id=<?php echo $id ?>" class="button"><i
-                    class="fa fa-pencil-square-o"></i>Actualizar</a>
+            <a href="edit-photo.php?id=<?php echo $id ?>" class="button">
+                <i class="fa fa-pencil-square-o"></i>
+                Actualizar
+            </a>
         </div>
     </div>
 </div>
 
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Footer.php') ?>
+<?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/views/Shared/Footer.php') ?>
